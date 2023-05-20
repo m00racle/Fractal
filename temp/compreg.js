c323eb326052febf8ecf054328be4f5af98684a0
mooracle <yanuar.prakosa@gmail.com>
2023-05-21 06:34:31 +0700

    add temp folder for code design template
    This is reference for future implementations

---
+++ temp/compreg.js
@@ -0,0 +1,7 @@
+const firedb = firebase.firestore();
+// find out how to fetch all data
+// maybe ask how to build simple html structure for inputs
+
+// querry the last company id
+
+// add new company doc in companies collection.
